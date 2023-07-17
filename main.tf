dc86617d4d242718bfae8a993d63aaaf73b8ca4d
Yadav <vijay.g.yadav@accenture.com>
2023-07-17 19:35:29 -0400

    fixed storage config

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
 
 }
 
-resource "azurerm_storage_account" "this" {
+resource "azurerm_storage_account" "storage" {
   name                     = "dt-sg-account"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
