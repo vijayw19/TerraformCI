fbc4ab699ba24b6f4d7a77ba7219ea6446df4a64
Yadav <vijay.g.yadav@accenture.com>
2023-07-17 19:55:01 -0400

    change the account name

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "dt-sg-account"
+  name                     = "devstgaccount"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
